0455d24b8147f0432931e89edb6b85e01da60021
Eric J. Ma <ericmajinglong@gmail.com>
2016-12-15 20:11:14 -0500

    updated bash script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-pandoc index.md -o index.html -H header.html --toc -c styling.css
-pandoc cli.md -o cli.html -H header --toc -c styling.css
+pandoc index.md -o index.html -H header.html --toc -c styling.css -s
+pandoc cli.md -o cli.html -H header --toc -c styling.css -s
 
 git add .
 git commit
