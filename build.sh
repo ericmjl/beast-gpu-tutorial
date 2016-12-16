74159f7ddfa2c3d87c91d2f69e0e6dac029222c9
Eric J. Ma <ericmajinglong@gmail.com>
2016-12-15 20:12:35 -0500

    created CLI instruction HTML page

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-# pandoc index.md -o index.html -H header.html --toc -c styling.css -s
+pandoc index.md -o index.html -H header.html --toc -c styling.css -s
 
-pandoc cli.md -o cli.html -H header --toc -c styling.css -s
+pandoc cli.md -o cli.html -H header.html --toc -c styling.css -s
 
 git add .
 git commit
